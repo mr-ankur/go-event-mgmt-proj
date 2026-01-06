0a4f4ca9addbc0558a1f3c3b371e57ac730a6471
Ankur Gupta <ankur11.kmr@gmail.com>
2026-01-06 15:08:36 +0530

    creating a sql database table

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"example.com/event-mgmt/db"
 	"example.com/event-mgmt/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)

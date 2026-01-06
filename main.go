1a75e19ec7abcf8669ba8fada1bce7b0194371e8
Ankur Gupta <ankur11.kmr@gmail.com>
2026-01-06 14:46:52 +0530

    testing resquests & fixing the post request

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"example.com/event-mgmt/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,9 +12,29 @@ func main() {
 
 	server.GET("/events", getEvents)
 
+	server.POST("/events", createEvent)
+
 	server.Run(":8090") // localhost:8090
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.BindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadGateway, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event Created Successfully!", "event": event})
 }
--- models/event.go
+++ models/event.go
@@ -4,10 +4,10 @@ import "time"
 
 type Event struct {
 	ID          int
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
+	Name        string    `bindings:"required"`
+	Description string    `bindings:"required"`
+	Location    string    `bindings:"required"`
+	DateTime    time.Time `bindings:"required"`
 	UserID      int
 }
 
@@ -16,4 +16,8 @@ var events = []Event{}
 func (e Event) Save() {
 	// later add it to a database
 	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+	return events
 }

dd47fa26e3296edbeb1d50e5c090fed8f4659a81
Ankur Gupta <ankur11.kmr@gmail.com>
2026-01-09 13:39:46 +0530

    cancelling registration

--- models/event.go
+++ models/event.go
@@ -135,3 +135,20 @@ func (e Event) Register(userId int64) error {
 
 	return err
 }
+
+func (e Event) CancelRegisteration(userId int64) error {
+	query := `
+		DELETE FROM registrations
+		WHERE event_id = ? AND user_id = ?
+	`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(e.ID, userId)
+
+	return err
+}
--- routes/register.go
+++ routes/register.go
@@ -35,5 +35,23 @@ func registerForEvent(context *gin.Context) {
 }
 
 func cancelRegisteration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
 
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	var event models.Event
+	event.ID = eventId
+
+	err = event.CancelRegisteration(userId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not cancel registration."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Registeration Cancelled!"})
 }
